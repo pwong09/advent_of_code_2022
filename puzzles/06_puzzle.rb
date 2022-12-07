6d8f016aca1fbeeab587759511ad95c3c52b3a1b
pwong09 <pwong09@gmail.com>
2022-12-06 22:04:30 -0800

    Day 6 solution

--- puzzles/06_puzzle.rb
+++ puzzles/06_puzzle.rb
@@ -2,6 +2,25 @@
 
 require '../elves_data'
 
-class SignalDevice
-  include ElvesData
+include ElvesData
+
+data = read_file('../input/06_input.txt', /''/)[0].split('')
+
+def find_marker(array, count)
+  array.each_index do |idx|
+    next if idx + 1 < count
+
+    chars = []
+    ((idx + 1 - count)..idx).each do |j|
+      chars.push(array[j]) unless chars.include?(array[j])
+    end
+
+    return idx + 1 if chars.length == count
+  end
 end
+
+puts 'find marker at 4 unique chars'
+puts find_marker(data, 4)
+
+puts 'find marker at 14 unique chars'
+puts find_marker(data, 14)
