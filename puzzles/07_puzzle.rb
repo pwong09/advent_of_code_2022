26fcf7c70aafba03a5bf71542f8886aee785977c
pwong09 <pwong09@gmail.com>
2022-12-07 20:06:07 -0800

    Day 07 refactor

--- puzzles/07_puzzle.rb
+++ puzzles/07_puzzle.rb
@@ -2,63 +2,105 @@
 
 require '../elves_data'
 
-$directories = {}
+class Node
+  attr_accessor :name, :parent
 
-class Folder
-  attr_accessor :file_size, :directories
-
-  def initialize(path:)
-    @path = path
-    @files = []
-    @subdirectories = []
-    $directories[path] = self
-    @file_size = 0
+  def initialize(parent, name)
+    @name = name
+    @parent = parent
   end
 
-  def add_directory(name:)
-    @subdirectories.push(Folder.new(path: @path.dup.push(name)))
-  end
-
-  def total_size
-    @subdirectories.map(&:total_size).sum + file_size
+  def children
+    {}
   end
 end
 
-raw_input = File.read('../input/07_input.txt').split("\n$")
-puts raw_input.class
-current_path = []
-Folder.new(path: [])
-raw_input.each do |part|
-  if part[0..2] == ' cd'
-    case rel_path = part.gsub(' cd ', '')
-    when '..'
-      current_path.pop
-    when '/'
-      current_path = []
+class FileNode < Node
+  attr_reader :size
+
+  def initialize(parent, name, size)
+    super(parent, name)
+    @size = size
+  end
+end
+
+class DirNode < Node
+  attr_reader :children
+
+  def initialize(parent, name)
+    super
+    @children = {}
+    @size = nil
+  end
+
+  def size
+    @size = @children.values.map(&:size).sum if @size.nil?
+
+    @size
+  end
+end
+
+def parse_ls(lines, cwd)
+  lines.each do |line|
+    arg, name = line.split(' ')
+
+    if arg == 'dir'
+      cwd.children[name] = DirNode.new(cwd, name)
     else
-      current_path.push(rel_path)
-    end
-  else
-    nodes = part.split("\n")[1..]
-    nodes.each do |node|
-      if node[0..2] == 'dir'
-        name = node.gsub('dir ', '')
-        $directories[current_path].add_directory(name: name)
-      else
-        size = node.split(' ')[0]
-        $directories[current_path].file_size += size.to_i
-      end
+      cwd.children[name] = FileNode.new(cwd, name, arg.to_i)
     end
   end
 end
 
-part1 = $directories.values.select { _1.total_size < 100_000 }.map(&:total_size).sum
-puts "Part 1: #{part1}"
+def crawl_size(max_size, root)
+  total = 0
 
-memory_to_delete = $directories[[]].total_size - 40_000_000
+  root_size = root.size
+  total += root_size if root_size <= max_size
 
-smallest_dir = $directories.values.map(&:total_size).sort.find { _1 >= memory_to_delete }
-puts "Part 2: #{smallest_dir}"
+  root.children.each_value do |child|
+    next if child.instance_of?(FileNode)
+
+    total += crawl_size(max_size, child)
+  end
+
+  total
+end
+
+def crawl_largest(required_size, root)
+  large_enough = []
+  large_enough << root.size if root.size >= required_size
+  large_enough << root.children.values.map { |c| crawl_largest(required_size, c) }
+end
+
+file = File.open('../input/07_input.txt')
+root = DirNode.new(nil, '/')
+cwd = root
+
+blocks = file.read.split('$').drop(2)
+file.close
+
+blocks.each do |block|
+  lines = block.strip.split("\n")
+  command = lines.shift
+
+  if command.start_with?('cd')
+    dir = command.delete_prefix('cd ')
+
+    cwd = if dir == '..'
+            cwd.parent
+          else
+            cwd = cwd.children[dir]
+          end
+  elsif command.start_with?('ls')
+    parse_ls(lines, cwd)
+  end
+end
+
+puts crawl_size(100_000, root)
+
+required = root.size + 30_000_000 - 70_000_000
+puts crawl_largest(required, root).flatten.sort.shift
 
 # pattern matching method
 def solve(input)
