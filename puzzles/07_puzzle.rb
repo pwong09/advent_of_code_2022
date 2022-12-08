e3cee87cc0d73058329d2d755d5d4cc18cbe84ae
pwong09 <pwong09@gmail.com>
2022-12-07 19:52:37 -0800

    Day 7 - second solution

--- puzzles/07_puzzle.rb
+++ puzzles/07_puzzle.rb
@@ -2,7 +2,7 @@
 
 require '../elves_data'
 
-@directories = {}
+$directories = {}
 
 class Folder
   attr_accessor :file_size, :directories
@@ -11,7 +11,7 @@ class Folder
     @path = path
     @files = []
     @subdirectories = []
-    @directories[path] = self
+    $directories[path] = self
     @file_size = 0
   end
 
@@ -43,19 +43,51 @@ raw_input.each do |part|
     nodes.each do |node|
       if node[0..2] == 'dir'
         name = node.gsub('dir ', '')
-        @directories[current_path].add_directory(name: name)
+        $directories[current_path].add_directory(name: name)
       else
         size = node.split(' ')[0]
-        @directories[current_path].file_size += size.to_i
+        $directories[current_path].file_size += size.to_i
       end
     end
   end
 end
 
-part1 = @directories.values.select { _1.total_size < 100_000 }.map(&:total_size).sum
+part1 = $directories.values.select { _1.total_size < 100_000 }.map(&:total_size).sum
 puts "Part 1: #{part1}"
 
-memory_to_delete = @directories[[]].total_size - 40_000_000
+memory_to_delete = $directories[[]].total_size - 40_000_000
 
-smallest_dir = @directories.values.map(&:total_size).sort.find { _1 >= memory_to_delete }
+smallest_dir = $directories.values.map(&:total_size).sort.find { _1 >= memory_to_delete }
 puts "Part 2: #{smallest_dir}"
+
+# pattern matching method
+def solve(input)
+  commands = File.read(input).split('$ ').reject(&:empty?).map { _1.split("\n").map(&:split) }
+  dirs = Hash.new(0)
+  cwd = []
+
+  commands.each do |cmd, *args|
+    case cmd
+    in ['ls']
+      total_contents_size = args.sum { |size, _fname| size.to_i }
+      (0..cwd.size).each do |i|
+        dirs[cwd[0, i]] += total_contents_size
+      end
+    in ['cd', '..'] then cwd.pop
+    in ['cd', '/'] then cwd = []
+    in ['cd', dir] then cwd.push(dir)
+    else warn "unknown command #{cmd}"
+    end
+  end
+  # Part 1
+  puts dirs.values.reject { _1 > 100_000 }.sum
+
+  # Part 2
+  total = 70_000_000
+  required = 30_000_000
+  free = total - dirs[[]]
+  required -= free
+  puts dirs.values.select { |size| size >= required }.min
+end
+
+puts solve('../input/07_input.txt')
